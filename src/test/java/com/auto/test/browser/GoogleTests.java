57daba61545c3c2962c0cf4067c399efa0488824
Lieu Le <lieule070999@gmail.com>
2022-12-03 15:55:59 +0700

    Config some assert method

--- src/test/java/com/auto/test/browser/GoogleTests.java
+++ src/test/java/com/auto/test/browser/GoogleTests.java
@@ -3,6 +3,7 @@ package com.auto.test.browser;
 import com.auto.page.browser.GooglePage;
 import com.auto.testng.TestListener;
 import com.auto.utils.Assertion;
+import com.google.gson.JsonObject;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -15,9 +16,20 @@ public class GoogleTests extends BrowserTestBase {
     public void Test_google_search() {
         googlePage.search("Selenium");
 
-//        assertion.assertTrue(false, "Verify user can create new item successfully");
+        assertion.assertTrue(false, "Verify true failed");
+        assertion.assertTrue(true, "Verify true passed");
 
-        assertion.assertEquals("aab", "aa", "test");
+        assertion.assertEquals("aa", "aa", "Verify string equal pass");
+        assertion.assertEquals("a", "aa", "Verify string equal failed");
+
+        assertion.assertNotEquals("a", "aa", "Verify string not equal pass");
+        assertion.assertEquals("aa","aa", "Verify string not equal failed");
+
+        assertion.assertNull(null, "Verify null passed");
+        assertion.assertNull("true", "Verify null failed");
+
+        assertion.assertNotNull("fdf", "Verify not null passed");
+        assertion.assertNotNull(null, "Verify not null failed");
 
         assertion.assertAll();
     }
